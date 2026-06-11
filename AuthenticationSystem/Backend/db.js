1cb7944f3ec5d8d9dfe02d6f1a2d4e138a0cbed9
santoshreddi-631 <reddisantosh772@gmail.com>
2026-06-11 15:13:57 +0530

    Add start script for deployment

--- AuthenticationSystem/Backend/db.js
+++ AuthenticationSystem/Backend/db.js
@@ -1,14 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
-import mysql2 from "mysql2/promise"
-const pool = mysql2.createPool({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    database:process.env.DB_NAME,
-    password:process.env.DB_PASSWORD,
-    waitForConnections:true,
-    connectionLimit:10,
-    queueLimit:0
-})
+
+import mysql from "mysql2/promise";
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 export default pool;
--- AuthenticationSystem/Backend/index.js
+++ AuthenticationSystem/Backend/index.js
@@ -4,37 +4,78 @@ import bcrypt from "bcrypt";
 import pool from "./db.js"
 const app = express();
 
-app.use(cors({origin:"*"}))
+app.use(cors({ origin: "*" }))
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/',(req,res)=>{
+
+try {
+    const connection = await pool.getConnection();
+    console.log("✅ MySQL Connected Successfully");
+    connection.release();
+} catch (error) {
+    console.error("❌ Database Connection Failed:", error.message);
+}
+
+
+app.get('/', (req, res) => {
     res.status(200).json("HI i am the default route...");
 })
-app.post('/login', async (req,res)=>{
-    const {email , password} = req.body;
-    console.log(req.body);
-    const user= await pool.query(`select * from registrationsDetails where email = '${email}'`)
-    const passwordHash = user[0][0].passwordHash;
-    const isPasswordCorrect = await bcrypt.compare(password,passwordHash);
-    if(isPasswordCorrect){
-        res.status(200).json({message:true});
-    }else{
-        res.status(401).json({message:false});
+
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    console.log("Email received:", email);
+
+    const [users] = await pool.query(
+      "SELECT * FROM users WHERE email = ?",
+      [email]
+    );
+
+    console.log("Users found:", users);
+
+    if (users.length === 0) {
+      return res.status(404).json({
+        message: "User not found"
+      });
     }
-    console.log(isPasswordCorrect);
-    console.log(user[0][0]);
+
+    const passwordHash = users[0].password;
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      passwordHash
+    );
+
+    if (!isPasswordCorrect) {
+      return res.status(401).json({
+        message: "Invalid Password"
+      });
+    }
+
+    return res.status(200).json({
+      message: "Login Success"
+    });
+
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server Error"
+    });
+  }
+});
+
+
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await pool.query(`insert into users(name,email,password) values('${name}','${email}','${hashedPassword}')`)
+    console.log(newUser);
+    res.status(200).json({ message: "New User Registered succesfully!" })
+
 })
 
-app.post('/register', async (req,res)=>{
-    const {name , email , password} = req.body;
-    const hashedPassword = await bcrypt.hash(password,10);
-    const newUser = await pool.query(`insert into registrationsDetails(name,email,passwordHash) values('${name}','${email}','${hashedPassword}')`)
-    console.log(newUser); 
-    res.status(200).json({message:"New User Registered succesfully!"})
-    
-})
-
-app.listen(3000,()=>{
-    console.log("Server is Started on 3000...");
+app.listen(process.env.PORT, () => {
+    console.log(`Server is Started on ${process.env.PORT}...`);
 })
