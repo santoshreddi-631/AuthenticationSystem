0aca3a191a4091e8ebd5b9193aa8c47c20f6c3e3
santoshreddi-631 <reddisantosh772@gmail.com>
2026-06-11 15:52:13 +0530

    Update frontend for deployment

--- AuthenticationSystem/Frontend/src/Utils/axiosClient.js
+++ AuthenticationSystem/Frontend/src/Utils/axiosClient.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const axiosCLient = axios.create({
-    baseURL:'http://localhost:3000'
+    baseURL:'https://authenticationsystem-fnxv.onrender.com/'
 })
 export default axiosCLient;
